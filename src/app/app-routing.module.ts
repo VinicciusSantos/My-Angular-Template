695f2f6f90a5511c4c198184cfa84c2ff8370cc0
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-10-31 09:09:01 -0300

    :boom: fix: Removendo Linhas Ineficientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,7 @@
-import { RotaNaoEncontradaComponent } from './pages/rota-nao-encontrada/rota-nao-encontrada.component';
-import { AuthGuard } from './guards/auth.guard';
-import { LoginComponent } from './pages/login/login.component';
-import { FavoritosComponent } from './pages/favoritos/favoritos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginGuard } from './guards/login.guard';
 
-export const mainRoutes: Routes = [
-  {
-    path: 'pokemons',
-    loadChildren: () =>
-      import('./pages/pokemons/pokemons.module').then(x => x.PokemonsModule),
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'favoritos',
-    component: FavoritosComponent,
-    canActivate: [AuthGuard],
-  },
-  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
-  { path: '**', component: RotaNaoEncontradaComponent },
-];
+export const mainRoutes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(mainRoutes, { useHash: true })],
