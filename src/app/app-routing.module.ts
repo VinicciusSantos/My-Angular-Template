819bd97a68f834c23f4471a5ebdcde8a03134c12
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-10-31 08:58:27 -0300

    :sparkles: feat: Adicionando esLint, Jest, Prettier, Stylelint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
+import { RotaNaoEncontradaComponent } from './pages/rota-nao-encontrada/rota-nao-encontrada.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './guards/login.guard';
 
-const routes: Routes = [];
+export const mainRoutes: Routes = [
+  {
+    path: 'pokemons',
+    loadChildren: () =>
+      import('./pages/pokemons/pokemons.module').then(x => x.PokemonsModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'favoritos',
+    component: FavoritosComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: '**', component: RotaNaoEncontradaComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(mainRoutes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+registerLocaleData(en);
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
